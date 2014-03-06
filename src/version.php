4bbf47eb98f3fffbac59ed4cc8597d173cad4393
Olli Puurunen <olli.puurunen@discendum.com>
2014-03-06 13:48:58 +0200

    Bump up the version number

--- src/version.php
+++ src/version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') or die();
 
-$plugin->version    = 2014011700;
+$plugin->version    = 2014030600;
 $plugin->requires   = 2011120511;
 $plugin->cron       = 86400;
 $plugin->component  = 'local_obf';
