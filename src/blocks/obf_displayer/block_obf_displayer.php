a082894ffcde7a5535e934f5ea1b4b76a314c69c
Jouni Suorsa <jouni.suorsa@discendum.com>
2015-06-24 14:51:24 +0300

    Added Open Badge Passport support to displayer block.
    Also fixed displayer block config checks.

--- src/blocks/obf_displayer/block_obf_displayer.php
+++ src/blocks/obf_displayer/block_obf_displayer.php
@@ -8,6 +8,10 @@ class block_obf_displayer extends block_base {
         $this->title = get_string('obf_displayer', 'block_obf_displayer');
     }
 
+    public function instance_allow_multiple() {
+      return true;
+    }
+
     public function get_content() {
         global $DB, $PAGE;
         if ($this->content !== null) {
@@ -23,46 +27,86 @@ class block_obf_displayer extends block_base {
 
         $assertions = $this->get_assertions($userid, $DB);
 
+        if (!empty($this->config)) {
+            if ($this->config->showmoz == false && $this->config->showobp == false && $this->config->showobf == true) {
+                $this->title = get_string('blocktitleobf', 'block_obf_displayer');
+            } else if ($this->config->showmoz == false && $this->config->showobp == true && $this->config->showobf == false) {
+                $this->title = get_string('blocktitleobp', 'block_obf_displayer');
+            } else if ($this->config->showmoz == true && $this->config->showobp == false && $this->config->showobf == false) {
+                $this->title = get_string('blocktitlemoz', 'block_obf_displayer');
+            } else {
+                $this->title = get_string('blocktitle', 'block_obf_displayer');
+            }
+        } else {
+            $this->title = get_string('blocktitle', 'block_obf_displayer');
+        }
 
         $this->content =  new stdClass;
         $this->content->text = '';
         $renderer = $PAGE->get_renderer('local_obf');
         if ($assertions !== false && count($assertions) > 0) {
-            $this->content->text = $renderer->render_user_assertions($assertions);
+            $this->content->text .= $renderer->render_user_assertions($assertions);
+        }
+        $providers = obf_backpack::get_providers();
+        foreach ($providers as $provider) {
+            $assertions = $this->get_backpack_assertions($userid, $DB, $provider);
+            if (count($assertions) > 0) {
+                $this->content->text .= $renderer->render_user_assertions($assertions);
+            }
         }
 
         return $this->content;
     }
     private function get_assertions($userid, $db) {
-        $cache = cache::make('block_obf_displayer', 'obf_assertions');
-        $assertions = $cache->get($userid);
+        if (empty($this->config) || !property_exists($this->config, 'showobf') || $this->config->showobf) {
+            $cache = cache::make('block_obf_displayer', 'obf_assertions');
+            $assertions = $cache->get($userid);
 
-        if (!$assertions) {
-            // Get user's badges in OBF
-            $assertions = new obf_assertion_collection();
-            if ($this->config->showobf) {
+            if (!$assertions) {
+                // Get user's badges in OBF
+                $assertions = new obf_assertion_collection();
                 try {
                     $client = obf_client::get_instance();
                     $assertions->add_collection(obf_assertion::get_assertions($client, null, $db->get_record('user', array('id' => $userid))->email ));
                 } catch(Exception $e) {
                     debugging('Getting OBF assertions for user id: ' . $userid . ' failed: ' . $e->getMessage());
                 }
-            }
 
-            if ($this->config->showbackpack) {
+                $assertions->toArray(); // This makes sure issuer objects are populated and cached
+                $cache->set($userid, $assertions );
+            }
+        } else {
+            $assertions = new obf_assertion_collection();
+        }
+        return $assertions;
+    }
+    private function get_backpack_assertions($userid, $db, $provider) {
+        $backpack = obf_backpack::get_instance_by_userid($userid, $db, $provider);
+        if ($backpack === false || count($backpack->get_group_ids()) == 0) {
+            return new obf_assertion_collection();
+        }
+        $showprop = 'show'.$backpack->get_providershortname();
+        if (empty($this->config) || !property_exists($this->config, $showprop) || $this->config->{$showprop}) {
+            $cache = cache::make('block_obf_displayer', 'obf_assertions_' . $backpack->get_providershortname());
+            $assertions = $cache->get($userid);
+
+            if (!$assertions) {
+                // Get user's badges in OBF
+                $assertions = new obf_assertion_collection();
                 try {
                     // Also get user's badges in Backpack, if user has backpack settings
-                    $backpack = obf_backpack::get_instance_by_userid($userid, $db);
                     if ($backpack !== false && count($backpack->get_group_ids()) > 0) {
                         $assertions->add_collection( $backpack->get_assertions() );
                     }
                 } catch(Exception $e) {
                     debugging('Getting backpack assertions for user id: ' . $userid . ' failed: ' . $e->getMessage());
                 }
-            }
 
-            $assertions->toArray(); // This makes sure issuer objects are populated and cached
-            $cache->set($userid, $assertions );
+                $assertions->toArray(); // This makes sure issuer objects are populated and cached
+                $cache->set($userid, $assertions );
+            }
+        } else {
+            $assertions = new obf_assertion_collection();
         }
         return $assertions;
     }
--- src/blocks/obf_displayer/edit_form.php
+++ src/blocks/obf_displayer/edit_form.php
@@ -7,10 +7,11 @@ class block_obf_displayer_edit_form extends block_edit_form {
         // Section header title according to language file.
         $mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
 
-        $mform->addElement('advcheckbox', 'config_showbackpack', get_string('showbackpack', 'block_obf_displayer'));
-        $mform->setDefault('config_showbackpack', 1);
         $mform->addElement('advcheckbox', 'config_showobf', get_string('showobf', 'block_obf_displayer'));
         $mform->setDefault('config_showobf', 1);
-
+        $mform->addElement('advcheckbox', 'config_showobp', get_string('showobp', 'block_obf_displayer'));
+        $mform->setDefault('config_showobp', 1);
+        $mform->addElement('advcheckbox', 'config_showmoz', get_string('showmoz', 'block_obf_displayer'));
+        $mform->setDefault('config_showmoz', 1);
     }
 }
