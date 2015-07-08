2f1f64496920030e3d083b9b7e8a2e7637286bc5
Jouni Suorsa <jouni.suorsa@discendum.com>
2015-07-08 12:02:12 +0300

    Version number bump.

--- src/local/obf/version.php
+++ src/local/obf/version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') or die();
 
-$plugin->version    = 2015070600;
+$plugin->version    = 2015070800;
 $plugin->requires   = 2011120511;
 $plugin->cron       = 86400;
 $plugin->component  = 'local_obf';
