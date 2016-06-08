0326ed00a345d9f5ab66ad54e7769f09c79c4615
Jouni Suorsa <jouni.suorsa@discendum.com>
2016-06-08 09:12:07 +0300

    Added backpack source tables for install-step. Switched mozilla backpack to use backpack.openbadges.org instead of beta.openbadges.org by default, admins have the option to change backpack locations.

--- src/local/obf/db/upgrade.php
+++ src/local/obf/db/upgrade.php
@@ -614,7 +614,7 @@ function xmldb_local_obf_upgrade($oldversion) {
             
             $backpacksources = array();
             $obj = new stdClass();
-            $obj->url = 'https://beta.openbadges.org/displayer/';
+            $obj->url = 'https://backpack.openbadges.org/displayer/';
             $obj->fullname = 'Backpack';
             $obj->shortname = 'moz';
             $obj->requirepersonaorg = 1;
