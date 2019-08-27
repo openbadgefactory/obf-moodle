a188351aadf222fc5d1fafd17de7686e8c754529
Mika Veljo <mika.veljo@obfsolutions.com>
2019-08-27 10:58:35 +0300

    Save changed user email

--- src/local/obf/classes/task/user_email_changer.php
+++ src/local/obf/classes/task/user_email_changer.php
@@ -2,6 +2,7 @@
 
 namespace local_obf\task;
 use \obf_backpack;
+use stdClass;
 
 class user_email_changer extends \core\task\scheduled_task {
 
@@ -19,9 +20,19 @@ class user_email_changer extends \core\task\scheduled_task {
 
         foreach ($users as $user) {
             $pack = obf_backpack::get_instance_by_userid($user, $DB);
+            $content = new stdClass();
             if($pack) {
-                if ($pack->get_email() != $records[$pack->get_user_id()]->email) {
+                $usermail = $records[$pack->get_user_id()]->email;
+                $record = $DB->get_record('local_obf_deleted_emails',
+                    array('user_id' => $pack->get_user_id(),'email' => $usermail));
+                if ($pack->get_email() != $usermail) {
                     if (!$pack->requires_email_verification()){
+                        if(!$record) {
+                            $content->user_id = $pack->get_user_id();
+                            $content->email = $usermail;
+                            $content->timestamp = time();
+                            $DB->insert_record('local_obf_deleted_emails', $content);
+                        }
                         $pack->disconnect();
                     }
                 }
